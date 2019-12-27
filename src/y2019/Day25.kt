0f7c80c3e22a810be70a1620106b268e218fa765
axelvondreden <axelvondreden@gmail.com>
2019-12-27 11:19:21 +0100

    2019.25

---
+++ src/y2019/Day25.kt
@@ -0,0 +1,29 @@
+package y2019
+
+import Day
+import Utils
+
+class Day25 : Day() {
+
+    override val input = Utils.readLongArray(2019, 25)
+
+    override fun solve1(): Int {
+        val comp = IntCodeComputer(input)
+        var last9 = ""
+        while (true) {
+            val reply = comp.run()
+            val c = reply.value.toChar()
+            last9 = last9.takeLast(8) + c
+            print(c)
+            if (last9 == "Command?\n") {
+                (readLine() + '\n').forEach {
+                    comp.addInput(it.toLong())
+                }
+            }
+        }
+    }
+
+    override fun solve2(): Int {
+        return 0
+    }
+}
