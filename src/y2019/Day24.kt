704fa7a4dce141151a498661266f575fe20c6ded
axelvondreden <axelvondreden@gmail.com>
2019-12-27 10:37:03 +0100

    2019.24.2

--- src/y2019/Day24.kt
+++ src/y2019/Day24.kt
@@ -11,7 +11,7 @@ class Day24 : Day() {
 
     override fun solve1(): Int {
         val states = mutableSetOf(input.toFlatString())
-        var new = step(input)
+        var new = step(input.copy())
         while (!states.contains(new.toFlatString())) {
             states.add(new.toFlatString())
             new = step(new)
@@ -20,7 +20,15 @@ class Day24 : Day() {
     }
 
     override fun solve2(): Int {
-        return 0
+        var map = mutableMapOf<Int, Array<CharArray>>()
+        for (i in -200..200) {
+            map[i] = Array(5) { CharArray(5) { '.' } }
+        }
+        map[0] = input.copy()
+        repeat(200) {
+            map = stepRecursive(map)
+        }
+        return map.values.sumBy { it.sumBy { it.count { it == '#' } } }
     }
 
     private fun step(map: Array<CharArray>): Array<CharArray> {
@@ -36,6 +44,130 @@ class Day24 : Day() {
             }
         }
         return new
+    }
+
+    private fun stepRecursive(map: MutableMap<Int, Array<CharArray>>): MutableMap<Int, Array<CharArray>> {
+        val minLvl = map.keys.min()!!
+        val maxLvl = map.keys.max()!!
+        val ref = mutableMapOf<Int, Array<CharArray>>()
+        for (i in minLvl..maxLvl) {
+            ref[i] = map[i]!!.copy()
+        }
+        for (lvl in minLvl..maxLvl) {
+            for (y in ref.getValue(lvl)[0].indices) {
+                for (x in ref.getValue(lvl).indices) {
+                    val n = countNeighborsRecursive(ref, lvl, x, y)
+                    if (ref.getValue(lvl)[x][y] == '#') {
+                        map.getValue(lvl)[x][y] = if (n == 1) '#' else '.'
+                    } else {
+                        map.getValue(lvl)[x][y] = if (n == 1 || n == 2) '#' else '.'
+                    }
+                }
+            }
+        }
+        return map
+    }
+
+    private fun countNeighborsRecursive(map: Map<Int, Array<CharArray>>, lvl: Int, x: Int, y: Int): Int {
+        var count = 0
+        val base = map.getValue(lvl)
+        when {
+            x == 0 && y == 0 -> {
+                count += if (base[1][0] == '#') 1 else 0
+                count += if (base[0][1] == '#') 1 else 0
+                count += if (map.containsKey(lvl - 1) && map.getValue(lvl - 1)[1][2] == '#') 1 else 0
+                count += if (map.containsKey(lvl - 1) && map.getValue(lvl - 1)[2][1] == '#') 1 else 0
+            }
+            x == 4 && y == 0 -> {
+                count += if (base[3][0] == '#') 1 else 0
+                count += if (base[4][1] == '#') 1 else 0
+                count += if (map.containsKey(lvl - 1) && map.getValue(lvl - 1)[2][1] == '#') 1 else 0
+                count += if (map.containsKey(lvl - 1) && map.getValue(lvl - 1)[3][2] == '#') 1 else 0
+            }
+            x == 0 && y == 4 -> {
+                count += if (base[1][4] == '#') 1 else 0
+                count += if (base[0][3] == '#') 1 else 0
+                count += if (map.containsKey(lvl - 1) && map.getValue(lvl - 1)[1][2] == '#') 1 else 0
+                count += if (map.containsKey(lvl - 1) && map.getValue(lvl - 1)[2][3] == '#') 1 else 0
+            }
+            x == 4 && y == 4 -> {
+                count += if (base[3][4] == '#') 1 else 0
+                count += if (base[4][3] == '#') 1 else 0
+                count += if (map.containsKey(lvl - 1) && map.getValue(lvl - 1)[3][2] == '#') 1 else 0
+                count += if (map.containsKey(lvl - 1) && map.getValue(lvl - 1)[2][3] == '#') 1 else 0
+            }
+            x == 0 -> {
+                count += if (base[0][y - 1] == '#') 1 else 0
+                count += if (base[0][y + 1] == '#') 1 else 0
+                count += if (base[1][y] == '#') 1 else 0
+                count += if (map.containsKey(lvl - 1) && map.getValue(lvl - 1)[1][2] == '#') 1 else 0
+            }
+            x == 4 -> {
+                count += if (base[4][y - 1] == '#') 1 else 0
+                count += if (base[4][y + 1] == '#') 1 else 0
+                count += if (base[3][y] == '#') 1 else 0
+                count += if (map.containsKey(lvl - 1) && map.getValue(lvl - 1)[3][2] == '#') 1 else 0
+            }
+            y == 0 -> {
+                count += if (base[x - 1][0] == '#') 1 else 0
+                count += if (base[x + 1][0] == '#') 1 else 0
+                count += if (base[x][1] == '#') 1 else 0
+                count += if (map.containsKey(lvl - 1) && map.getValue(lvl - 1)[2][1] == '#') 1 else 0
+            }
+            y == 4 -> {
+                count += if (base[x - 1][4] == '#') 1 else 0
+                count += if (base[x + 1][4] == '#') 1 else 0
+                count += if (base[x][3] == '#') 1 else 0
+                count += if (map.containsKey(lvl - 1) && map.getValue(lvl - 1)[2][3] == '#') 1 else 0
+            }
+            x == 2 && y == 1 -> {
+                count += if (base[1][1] == '#') 1 else 0
+                count += if (base[3][1] == '#') 1 else 0
+                count += if (base[2][0] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[0][0] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[1][0] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[2][0] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[3][0] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[4][0] == '#') 1 else 0
+            }
+            x == 2 && y == 3 -> {
+                count += if (base[1][3] == '#') 1 else 0
+                count += if (base[3][3] == '#') 1 else 0
+                count += if (base[2][4] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[0][4] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[1][4] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[2][4] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[3][4] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[4][4] == '#') 1 else 0
+            }
+            x == 1 && y == 2 -> {
+                count += if (base[1][1] == '#') 1 else 0
+                count += if (base[1][3] == '#') 1 else 0
+                count += if (base[0][2] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[0][0] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[0][1] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[0][2] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[0][3] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[0][4] == '#') 1 else 0
+            }
+            x == 3 && y == 2 -> {
+                count += if (base[3][1] == '#') 1 else 0
+                count += if (base[3][3] == '#') 1 else 0
+                count += if (base[4][2] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[4][0] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[4][1] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[4][2] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[4][3] == '#') 1 else 0
+                count += if (map.containsKey(lvl + 1) && map.getValue(lvl + 1)[4][4] == '#') 1 else 0
+            }
+            x != 2 || y != 2 -> {
+                count += if (base[x - 1][y] == '#') 1 else 0
+                count += if (base[x + 1][y] == '#') 1 else 0
+                count += if (base[x][y - 1] == '#') 1 else 0
+                count += if (base[x][y + 1] == '#') 1 else 0
+            }
+        }
+        return count
     }
 
     private fun Array<CharArray>.countNeighbors(x: Int, y: Int): Int {
