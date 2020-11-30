8285e1658208a84f56b5c1ed55e35164d474d18c
axl <axelvondreden@gmail.com>
2020-11-30 16:13:11 +0100

    2016.09.2

--- src/main/kotlin/y2016/Day09.kt
+++ src/main/kotlin/y2016/Day09.kt
@@ -6,9 +6,45 @@ class Day09 : Day() {
 
     override val input = Utils.readString(2016, 9)
 
-    override fun solve1() = input.decode().length
+    override fun solve1() = input.decodeCount()
 
-    override fun solve2() = 0
+    override fun solve2() = input.decodeCountRec()
+
+    private fun String.decodeCount(): Long {
+        var count = 0L
+        var index = 0
+        while (index < length) {
+            if (get(index) == '(') {
+                val endIndex = indexOf(')', index)
+                val marker = parseMarker(substring(index + 1, endIndex))
+                val s = substring(endIndex + 1, endIndex + marker.chars + 1)
+                count += s.length * marker.repeats
+                index += ((endIndex - index) + marker.chars + 1)
+            } else {
+                count++
+                index++
+            }
+        }
+        return count
+    }
+
+    private fun String.decodeCountRec(): Long {
+        var count = 0L
+        var index = 0
+        while (index < length) {
+            if (get(index) == '(') {
+                val endIndex = indexOf(')', index)
+                val marker = parseMarker(substring(index + 1, endIndex))
+                val s = substring(endIndex + 1, endIndex + marker.chars + 1)
+                count += s.decodeCountRec() * marker.repeats
+                index += ((endIndex - index) + marker.chars + 1)
+            } else {
+                count++
+                index++
+            }
+        }
+        return count
+    }
 
     private fun String.decode(): String {
         var newString = ""
