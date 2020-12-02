09c06e15e3779327224e8db91bc803ff7f45b6f4
axl <axelvondreden@gmail.com>
2020-12-02 08:13:57 +0100

    2016.16.2

--- src/main/kotlin/y2016/Day16.kt
+++ src/main/kotlin/y2016/Day16.kt
@@ -10,29 +10,36 @@ class Day16 : Day() {
 
     override fun solve1(): String {
         var string = input
-        while (string.length < maxLength) {
+        while (string.length < maxLength1) {
             string = string.dragonCurve()
         }
-        return string.substring(0, maxLength).checksum()
+        return string.substring(0, maxLength1).checksum()
 
     }
 
-    override fun solve2() = 0
+    override fun solve2(): String {
+        var string = input
+        while (string.length < maxLength2) {
+            string = string.dragonCurve()
+        }
+        return string.substring(0, maxLength2).checksum()
+    }
 
     private fun String.dragonCurve() = this + "0" + this.reversed().map { if (it == '0') '1' else '0' }.joinToString("")
 
     private fun String.checksum(): String {
-        var checksum = ""
+        val checksum = StringBuilder()
         for (i in 0 until length step 2) {
-            checksum += if (get(i) == get(i + 1)) '1' else '0'
+            checksum.append(if (get(i) == get(i + 1)) '1' else '0')
         }
         if (checksum.length % 2 == 0) {
-            return checksum.checksum()
+            return checksum.toString().checksum()
         }
-        return checksum
+        return checksum.toString()
     }
 
     companion object {
-        const val maxLength = 272
+        const val maxLength1 = 272
+        const val maxLength2 = 35651584
     }
 }
