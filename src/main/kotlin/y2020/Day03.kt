d4d4ad9f173507dcd35ace8f2da2ba6ae4648a82
axl <axelvondreden@gmail.com>
2020-12-03 11:15:13 +0100

    2020.03

---
+++ src/main/kotlin/y2020/Day03.kt
@@ -0,0 +1,27 @@
+package y2020
+
+import Day
+import Point
+import Utils
+
+class Day03 : Day() {
+
+    override val input = Utils.readCharMatrix(2020, 3)
+
+    override fun solve1() = countTrees(3, 1)
+
+    override fun solve2() = countTrees(1, 1) * countTrees(3, 1) * countTrees(5, 1)*
+            countTrees(7, 1) * countTrees(1, 2)
+
+    private fun countTrees(right: Int, down: Int): Long {
+        var position = Point(0, 0)
+        var trees = 0L
+        while (position.y < input[0].size) {
+            if (input[position.x % input.size][position.y] == '#') {
+                trees++
+            }
+            position = position.plus(right, down)
+        }
+        return trees
+    }
+}
