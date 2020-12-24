23b9e1d585cd47143d56fa2930a8c6b44f43fcd3
axl <axelvondreden@gmail.com>
2020-12-24 13:34:06 +0100

    2020.23

---
+++ src/main/kotlin/y2020/Day23.kt
@@ -0,0 +1,73 @@
+package y2020
+
+import Day
+import utils.IO
+
+
+class Day23 : Day() {
+
+    override val input = IO.readString(2020, 23)
+
+    override fun solve1() = Cups(input).allMoves(100).toString()
+
+    override fun solve2() = Cups(input, 1000000)
+        .allMoves(10000000)
+        .nextAsList(2)
+        .map { it.value.toLong() }
+        .reduce { a, b -> a * b }
+
+    private class Cups(list: String, amount: Int = list.length) {
+        val cups = List(amount + 1) { Cup(it) }
+        var current = cups[list.first().toString().toInt()]
+
+        init {
+            val orderedCups = list.map { it.toString().toInt() }.plus((list.length + 1..amount))
+            orderedCups.map { cups[it] }
+                .fold(cups[list.last().toString().toInt()]) { prev, cup -> cup.also { prev.next = cup } }
+            cups[orderedCups.last()].next = cups[orderedCups.first()]
+        }
+
+        fun allMoves(rounds: Int): Cup {
+            repeat(rounds) { playRound() }
+            return cups[1]
+        }
+
+        private fun playRound() {
+            val next3 = current.nextAsList(3)
+            val destination = calculateDestination(next3.map { it.value }.toSet())
+            moveCups(next3, destination)
+            current = current.next
+        }
+
+        private fun moveCups(cupsToInsert: List<Cup>, destination: Cup) {
+            val prevDest = destination.next
+            current.next = cupsToInsert.last().next
+            destination.next = cupsToInsert.first()
+            cupsToInsert.last().next = prevDest
+        }
+
+        private fun calculateDestination(exempt: Set<Int>): Cup {
+            var dest = current.value - 1
+            while (dest in exempt || dest == 0) {
+                dest = if (dest == 0) cups.size - 1 else dest - 1
+            }
+            return cups[dest]
+        }
+    }
+
+    private class Cup(val value: Int) {
+
+        lateinit var next: Cup
+
+        fun nextAsList(n: Int) = (1..n).runningFold(this) { cur, _ -> cur.next }.drop(1)
+
+        override fun toString() = buildString {
+            var current = this@Cup.next
+            while (current != this@Cup) {
+                append(current.value.toString())
+                current = current.next
+            }
+        }
+    }
+
+}
