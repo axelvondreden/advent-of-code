e75e94d0041620f427da23297dc6571185e7cbd1
axl <axelvondreden@gmail.com>
2021-01-05 14:03:47 +0100

    2017.17

--- src/main/kotlin/y2017/Day17.kt
+++ src/main/kotlin/y2017/Day17.kt
@@ -7,27 +7,24 @@ class Day17 : Day(2017, 17) {
     override val input = readString().toInt()
 
     override fun solve1(): Int {
-        val buffer = ArrayDeque<Int>(2018).apply { add(0) }
-        var currentPosition = 0
-        var counter = 1
-        while (counter <= 2017) {
-            currentPosition = (currentPosition + input + 1) % buffer.size
-            buffer.add(currentPosition, counter)
-            counter++
-        }
+        val buffer = fillBuffer(2017)
         return buffer[buffer.indexOf(2017) + 1]
     }
 
     override fun solve2(): Int {
-        val buffer = ArrayDeque<Int>(50_000_000).apply { add(0) }
+        val buffer = fillBuffer(50000000)
+        return buffer[buffer.indexOf(0) + 1]
+    }
+
+    private fun fillBuffer(size: Int): ArrayDeque<Int> {
+        val buffer = ArrayDeque<Int>(size).apply { add(0) }
         var currentPosition = 0
         var counter = 1
-        while (counter <= 50_000_000) {
+        while (counter <= size) {
             currentPosition = (currentPosition + input + 1) % buffer.size
             buffer.add(currentPosition, counter)
             counter++
-            if (counter % 100000 == 0) println(counter / 50_000_000.0)
         }
-        return buffer[buffer.indexOf(0) + 1]
+        return buffer
     }
 }
