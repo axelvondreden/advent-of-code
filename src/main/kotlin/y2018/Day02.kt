c1cb981d619f6209e5ea9ace51097c1d32047707
axl <axelvondreden@gmail.com>
2021-01-11 16:17:01 +0100

    2018.02

---
+++ src/main/kotlin/y2018/Day02.kt
@@ -0,0 +1,30 @@
+package y2018
+
+import Day
+
+class Day02 : Day(2018, 2) {
+
+    override val input = readStrings()
+
+    override fun solve1() = input.count { id -> id.groupBy { it }.any { it.value.size == 2 } } *
+            input.count { id -> id.groupBy { it }.any { it.value.size == 3 } }
+
+    override fun solve2(): String {
+        val similarIDs = input.findSimilarIDs()
+        return similarIDs.first.indices
+            .filter { similarIDs.first[it] == similarIDs.second[it] }
+            .map { similarIDs.first[it] }.joinToString("")
+    }
+
+    private fun List<String>.findSimilarIDs(): Pair<String, String> {
+        forEach { id1 ->
+            forEach { id2 ->
+                if (id1 != id2) {
+                    val diffs = id1.indices.map { id1[it] == id2[it] }.count { !it }
+                    if (diffs == 1) return id1 to id2
+                }
+            }
+        }
+        error("oh no!")
+    }
+}
