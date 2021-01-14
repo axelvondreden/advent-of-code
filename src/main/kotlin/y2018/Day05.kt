6577f68fbb6b05fad9587e7b7241b35b15634c2d
axl <axelvondreden@gmail.com>
2021-01-14 11:42:21 +0100

    2018.05

---
+++ src/main/kotlin/y2018/Day05.kt
@@ -0,0 +1,33 @@
+package y2018
+
+import Day
+
+class Day05 : Day(2018, 5) {
+
+    override val input = readString()
+
+    override fun solve1() = input.fullReact().length
+
+    override fun solve2() = ('a'..'z').map {
+        input.replace(it.toString(), "", ignoreCase = true).fullReact().length
+    }.minOrNull()!!
+
+    private fun String.fullReact(): String {
+        var poly = this
+        var newPoly = poly.react()
+        while (newPoly.length != poly.length) {
+            poly = newPoly
+            newPoly = poly.react()
+        }
+        return newPoly
+    }
+
+    private fun String.react(): String {
+        val index = (0 until length - 1).firstOrNull {
+            val c1 = get(it)
+            val c2 = get(it + 1)
+            c1.equals(c2, ignoreCase = true) && c1 != c2
+        }
+        return index?.let { removeRange(it, index + 2) } ?: this
+    }
+}
