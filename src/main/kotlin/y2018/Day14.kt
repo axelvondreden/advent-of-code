6903163e51d4aed53c27a79064e09f9af00bc1d8
axl <axel@vondreden.com>
2021-12-03 14:50:20 +0100

    2018.14

---
+++ src/main/kotlin/y2018/Day14.kt
@@ -0,0 +1,49 @@
+package y2018
+
+import Day
+
+class Day14 : Day(2018, 14) {
+
+    override val input = readString().toInt()
+
+    override fun solve1(): String {
+        val scores = mutableListOf(3, 7)
+        var index1 = 0
+        var index2 = 1
+        while (scores.size < input + 10) {
+            val rec1 = scores[index1]
+            val rec2 = scores[index2]
+            scores.addAll((rec1 + rec2).toString().map { it.toString().toInt() })
+            index1 += rec1 + 1
+            index1 %= scores.size
+            index2 += rec2 + 1
+            index2 %= scores.size
+        }
+        return scores.takeLast(10).joinToString("")
+    }
+
+    override fun solve2(): Int {
+        val scores = mutableListOf(3, 7)
+        var index1 = 0
+        var index2 = 1
+        while (true) {
+            val rec1 = scores[index1]
+            val rec2 = scores[index2]
+            (rec1 + rec2).toString().map { it.toString().toInt() }.forEach {
+                scores += it
+                if (scores.check()) {
+                    return scores.size - input.toString().length
+                }
+            }
+            index1 += rec1 + 1
+            index1 %= scores.size
+            index2 += rec2 + 1
+            index2 %= scores.size
+        }
+    }
+
+    private val checks = input.toString()
+    private fun List<Int>.check(): Boolean {
+        return takeLast(checks.length).joinToString("") == checks
+    }
+}
