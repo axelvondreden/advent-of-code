be428983305299b24dd59dea0ad72b41a40ab084
axl <axel@vondreden.com>
2022-12-07 02:30:31 +0100

    2022.5

---
+++ src/main/kotlin/y2022/Day05.kt
@@ -0,0 +1,58 @@
+package y2022
+
+import Day
+import java.util.Stack
+
+class Day05 : Day(2022, 5) {
+
+    override val input = readStrings()
+    private val stackLines = input.takeWhile { it.isNotEmpty() }
+    private val instructions = input.dropWhile { it.isNotEmpty() }.drop(1).map {
+        val s = it.split(" ")
+        Instruction(s[3].toInt(), s[5].toInt(), s[1].toInt())
+    }
+
+    override fun solve1(): String {
+        val stacks = Stacks(stackLines)
+        instructions.forEach { stacks.move(it) }
+        return stacks.getTopChars()
+    }
+
+    override fun solve2(): String {
+        val stacks = Stacks(stackLines)
+        instructions.forEach { stacks.move2(it) }
+        return stacks.getTopChars()
+    }
+
+    private data class Instruction(val source: Int, val target: Int, val amount: Int)
+
+    private class Stacks(lines: List<String>) {
+        private val stacks: Array<Stack<Char>>
+
+        init {
+            val map = lines.last().mapIndexedNotNull { index, c -> if (c.isDigit()) c.digitToInt() to index else null }.toMap()
+            stacks = Array(map.size) { Stack() }
+            lines.dropLast(1).forEach { line ->
+                map.forEach { (c, i) -> line.getOrNull(i)?.let { if (it.isLetter()) stacks[c - 1].add(0, it) } }
+            }
+        }
+
+        fun move(inst: Instruction) {
+            repeat(inst.amount) {
+                stacks[inst.target - 1].push(stacks[inst.source - 1].pop())
+            }
+        }
+
+        fun move2(inst: Instruction) {
+            val tempStack = Stack<Char>()
+            repeat(inst.amount) {
+                tempStack.push(stacks[inst.source - 1].pop())
+            }
+            repeat(inst.amount) {
+                stacks[inst.target - 1].push(tempStack.pop())
+            }
+        }
+
+        fun getTopChars() = stacks.joinToString("") { it.peek().toString() }
+    }
+}
