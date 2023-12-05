d14c2f4ebbd766db7a9dd0fa6188752ff4f4fc34
axelvondreden <axelvondreden@gmail.com>
2023-12-05 16:57:19 +0100

    2023.04

--- src/main/kotlin/y2023/Day03.kt
+++ src/main/kotlin/y2023/Day03.kt
@@ -6,7 +6,48 @@ class Day03 : Day(2023, 3) {
 
     override val input = readCharMatrix()
 
-    override fun solve1() = 0
+    override fun solve1(): Int {
+        val nrs = getPartNumbers()
+        return nrs.filter { it.adjacent }.sumOf { it.nr }
+    }
 
     override fun solve2() = 0
+
+    private data class Partnumber(val nr: Int, val x: Int, val y: Int, val adjacent: Boolean) {
+        val length get() = nr.toString().length
+    }
+
+    private fun getPartNumbers(): List<Partnumber> {
+        val list = mutableListOf<Partnumber>()
+        for (y in input[0].indices) {
+            var x = 0
+            while (x < input.size) {
+                var nr = ""
+                while (x < input.size && !input[x][y].isDigit()) {
+                    x++
+                }
+                while (x < input.size && input[x][y].isDigit()) {
+                    nr += input[x][y]
+                    x++
+                }
+                if (nr.isNotBlank()) {
+                    list += Partnumber(nr.toInt(), x - nr.length, y, partHasAdjacentSymbol(x - nr.length, y, nr.length))
+                }
+            }
+        }
+        return list
+    }
+
+    private fun partHasAdjacentSymbol(x: Int, y: Int, length: Int): Boolean {
+        for (xx in x - 1..x + 1 + length) {
+            for (yy in y - 1..y + 1) {
+                if (xx >= 0 && yy >= 0 && xx < input.size && yy < input[0].size) {
+                    if (!input[xx][yy].isDigit() && input[xx][yy] != '.') {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
 }
---
+++ src/main/kotlin/y2023/Day04.kt
@@ -0,0 +1,36 @@
+package y2023
+
+import Day
+import kotlin.math.pow
+
+class Day04 : Day(2023, 4) {
+
+    override val input = readStrings()
+
+    private val games = input.map { line ->
+        val split = line.removePrefix("Card ").split(":")
+        val split2 = split[1].trim().split("|")
+        Game(
+            nr = split[0].trim().toInt(),
+            winners = split2[0].trim().split(Regex("\\s+")).map { it.toInt() },
+            numbers = split2[1].trim().split(Regex("\\s+")).map { it.toInt() }
+        )
+    }
+
+    override fun solve1() = games.sumOf { it.score }
+
+    override fun solve2(): Int {
+        val array = IntArray(games.size) { 1 }
+        games.forEachIndexed { index, game ->
+            for (i in index + 1..index + game.winnerCount) {
+                array[i] += array[index]
+            }
+        }
+        return array.sum()
+    }
+
+    private data class Game(val nr: Int, val winners: List<Int>, val numbers: List<Int>) {
+        val winnerCount get() = numbers.count { it in winners }
+        val score get() = 2.0.pow(winnerCount.toDouble() - 1).toInt()
+    }
+}
