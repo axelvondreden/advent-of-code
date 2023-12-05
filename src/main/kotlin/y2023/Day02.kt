8d96cd709ca08e81cc6340a1dc83fd32c9579ddf
axelvondreden <axelvondreden@gmail.com>
2023-12-05 07:24:22 +0100

    2023.02

---
+++ src/main/kotlin/y2023/Day02.kt
@@ -0,0 +1,35 @@
+package y2023
+
+import Day
+
+class Day02 : Day(2023, 2) {
+
+    override val input = readStrings()
+
+    private val games = input.map { game ->
+        val s1 = game.split(": ")
+        val id = s1[0].removePrefix("Game ").toInt()
+        val s2 = s1[1].split("; ")
+        val rounds = s2.map { round ->
+            val vals = round.split(", ")
+            val r = vals.firstOrNull { it.endsWith("red") }?.split(" ")?.get(0)?.toInt() ?: 0
+            val g = vals.firstOrNull { it.endsWith("green") }?.split(" ")?.get(0)?.toInt() ?: 0
+            val b = vals.firstOrNull { it.endsWith("blue") }?.split(" ")?.get(0)?.toInt() ?: 0
+            Gameround(r, g, b)
+        }
+        Game(id, rounds)
+    }
+
+    override fun solve1() = games.filter { it.isPossible(12, 13, 14) }.sumOf { it.id }
+
+    override fun solve2() = games.sumOf { it.getLowestProduct() }
+    
+    private data class Game(val id: Int, val rounds: List<Gameround>) {
+
+        fun isPossible(r: Int, g: Int, b: Int) = rounds.none { it.r > r || it.g > g || it.b > b }
+
+        fun getLowestProduct() = rounds.maxOf { it.r } * rounds.maxOf { it.g } * rounds.maxOf { it.b }
+    }
+    
+    private data class Gameround(val r: Int, val g: Int, val b: Int)
+}
