9da0231705fc3637810fe85a3ffa4405f1bed3fd
axelvondreden <axelvondreden@gmail.com>
2023-12-08 12:20:52 +0100

    changes

--- src/main/kotlin/y2023/Day08.kt
+++ src/main/kotlin/y2023/Day08.kt
@@ -2,11 +2,54 @@ package y2023
 
 import Day
 
-class Day08 : Day<List<String>>(2023, 8) {
+class Day08 : Day<Day08.RLMap>(2023, 8) {
 
-    override fun List<String>.parse() = this
+    override fun List<String>.parse(): RLMap {
+        val path = first()
+        val nodes = drop(2).associate {
+            val s = it.split(" = ")
+            val s2 = s[1].drop(1).dropLast(1).split(", ")
+            s[0] to (s2[0] to s2[1])
+        }
+        return RLMap(path, nodes)
+    }
 
-    override fun solve1(input: List<String>) = 0
+    override fun solve1(input: RLMap) = input.countSteps()
 
-    override fun solve2(input: List<String>) = 0
+    override fun solve2(input: RLMap) = input.countGhostSteps()
+
+    data class RLMap(val path: String, val nodes: Map<String, Pair<String, String>>) {
+
+        fun countSteps(): Int {
+            var current = "AAA"
+            var count = 0
+            while (current != "ZZZ") {
+                val nextDir = path[count % path.length]
+                current = if (nextDir == 'R') {
+                    nodes[current]!!.second
+                } else {
+                    nodes[current]!!.first
+                }
+                count++
+            }
+            return count
+        }
+
+        fun countGhostSteps(): Int {
+            val current = nodes.keys.filter { it.endsWith('A') }.toTypedArray()
+            var count = 0
+            while (!current.all { it.endsWith('Z') }) {
+                val nextDir = path[count % path.length]
+                current.indices.forEach {
+                    current[it] = if (nextDir == 'R') {
+                        nodes[current[it]]!!.second
+                    } else {
+                        nodes[current[it]]!!.first
+                    }
+                }
+                count++
+            }
+            return count
+        }
+    }
 }
