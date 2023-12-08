8bc41d3abcf607e495759ca7a23023904022cef2
axl <axelvondreden@github.com>
2023-12-08 13:38:33 +0100

    c

--- src/main/kotlin/y2015/Day06.kt
+++ src/main/kotlin/y2015/Day06.kt
@@ -13,7 +13,7 @@ class Day06 : Day<List<List<String>>>(2015, 6) {
             when (line[0]) {
                 "on" -> bGrid.on(Point(line[1]), Point(line[2]))
                 "off" -> bGrid.off(Point(line[1]), Point(line[2]))
-                "utils.toggle" -> bGrid.toggle(Point(line[1]), Point(line[2]))
+                "toggle" -> bGrid.toggle(Point(line[1]), Point(line[2]))
             }
         }
         return bGrid.sumOf { booleans -> booleans.count { it } }
@@ -25,7 +25,7 @@ class Day06 : Day<List<List<String>>>(2015, 6) {
             when (line[0]) {
                 "on" -> iGrid.inc(Point(line[1]), Point(line[2]), 1)
                 "off" -> iGrid.dec(Point(line[1]), Point(line[2]), 1)
-                "utils.toggle" -> iGrid.inc(Point(line[1]), Point(line[2]), 2)
+                "toggle" -> iGrid.inc(Point(line[1]), Point(line[2]), 2)
             }
         }
         return iGrid.sumOf { ints -> ints.sum() }
