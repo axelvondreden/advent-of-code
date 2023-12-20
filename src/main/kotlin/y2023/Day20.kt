3174f89bc3e0f4ab81e90b70d3b7bb52fb988ecf
axl <axelvondreden@github.com>
2023-12-20 15:15:19 +0100

    changes

--- src/main/kotlin/y2023/Day20.kt
+++ src/main/kotlin/y2023/Day20.kt
@@ -10,18 +10,22 @@ class Day20 : Day<List<String>>(2023, 20) {
 
     override suspend fun solve2(input: List<String>) = 0
 
+    private fun pressButton(modules: List<Module>) {
+        modules.first { it.name == "broadcaster" }.receive(modules, "", Pulse.LOW)
+    }
+
     sealed class Module(val name: String, val inputs: List<String>, val outputs: List<String>) {
 
-        abstract fun receive(modules: List<Module>, pulse: Pulse)
+        abstract fun receive(modules: List<Module>, from: String, pulse: Pulse)
 
         fun send(modules: List<Module>, pulse: Pulse) {
             outputs.forEach { name ->
-                modules.first { it.name == name }.receive(modules, pulse)
+                modules.first { it.name == name }.receive(modules, this.name, pulse)
             }
         }
 
-        class FlipFlopModule(name: String, descendants: List<String>, var on: Boolean = false) : Module(name, descendants) {
-            override fun receive(modules: List<Module>, pulse: Pulse) {
+        class FlipFlopModule(name: String, inputs: List<String>, outputs: List<String>, var on: Boolean = false) : Module(name, inputs, outputs) {
+            override fun receive(modules: List<Module>, from: String, pulse: Pulse) {
                 if (pulse == Pulse.LOW) {
                     on = !on
                     send(modules, if (on) Pulse.HIGH else Pulse.LOW)
@@ -29,12 +33,17 @@ class Day20 : Day<List<String>>(2023, 20) {
             }
         }
 
-        class ConjunctionModel(name: String, descendants: List<String>) : Module(name, descendants) {
+        class ConjunctionModel(name: String, inputs: List<String>, outputs: List<String>) : Module(name, inputs, outputs) {
+            private val buffer = inputs.associateWith { Pulse.LOW }.toMutableMap()
 
+            override fun receive(modules: List<Module>, from: String, pulse: Pulse) {
+                buffer[from] = pulse
+                send(modules, if (buffer.values.all { it == Pulse.HIGH }) Pulse.LOW else Pulse.HIGH)
+            }
         }
 
-        class Broadcasterodule(name: String, descendants: List<String>) : Module(name, descendants) {
-            override fun receive(modules: List<Module>, pulse: Pulse) {
+        class Broadcasterodule(name: String, inputs: List<String>, outputs: List<String>) : Module(name, inputs, outputs) {
+            override fun receive(modules: List<Module>, from: String, pulse: Pulse) {
                 send(modules, pulse)
             }
         }
