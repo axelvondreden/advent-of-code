7d6ece75ecd1dc0e5f56c54f2735a10963cbd566
axelvondreden <axelvondreden@gmail.com>
2023-12-22 22:45:30 +0100

    changes

--- src/main/kotlin/y2016/Day01.kt
+++ src/main/kotlin/y2016/Day01.kt
@@ -1,10 +1,14 @@
 package y2016
 
 import Day
+import androidx.compose.ui.graphics.Color
+import runner.compose.Tile
+import runner.compose.Viz
 import utils.Dir
 import utils.Point
 import utils.Turn
 import kotlin.math.abs
+import kotlin.math.max
 
 class Day01 : Day<List<Day01.Step>>(2016, 1) {
 
@@ -45,6 +49,78 @@ class Day01 : Day<List<Day01.Step>>(2016, 1) {
         }
         return 0
     }
+
+    override fun initViz1(input: List<Step>) = Viz(width = 10).apply {
+        info["Distance"] = "0"
+    }
+
+    override fun initViz2(input: List<Step>) = Viz(width = 10).apply {
+        info["Distance"] = "0"
+    }
+
+    override suspend fun solve1Visualized(input: List<Step>, onProgress: suspend (Viz) -> Unit): Long {
+        val points = mutableSetOf(Point(0, 0))
+        var dir = Dir.UP
+        input.forEachIndexed { index, step ->
+            dir = dir.turn(step.turn)
+            points += when (dir) {
+                Dir.LEFT -> points.last() left step.amount
+                Dir.RIGHT -> points.last() right step.amount
+                Dir.UP -> points.last() up step.amount
+                Dir.DOWN -> points.last() down step.amount
+            }
+            val viz = getMapViz((index + 1).toDouble() / input.size, points.last(), points).apply {
+                info["Distance"] = points.first().distance(points.last()).toString()
+            }
+            onProgress(viz)
+        }
+        return abs(points.last().x) + abs(points.last().y)
+    }
+
+    override suspend fun solve2Visualized(input: List<Step>, onProgress: suspend (Viz) -> Unit): Long {
+        var point = Point(0, 0)
+        var dir = Dir.UP
+        val visited = mutableSetOf<Point>()
+        input.forEachIndexed { index, step ->
+            dir = dir.turn(step.turn)
+            repeat(step.amount) {
+                point = when (dir) {
+                    Dir.LEFT -> point left 1
+                    Dir.RIGHT -> point right 1
+                    Dir.UP -> point up 1
+                    Dir.DOWN -> point down 1
+                }
+                val viz = getMapViz((index + 1).toDouble() / input.size, point, visited).apply {
+                    info["Distance"] = visited.first().distance(visited.last()).toString()
+                }
+                onProgress(viz)
+                if (!visited.add(Point(point.x, point.y))) return abs(point.x) + abs(point.y)
+            }
+        }
+        return 0
+    }
+
+    private fun getMapViz(progress: Double, location: Point, points: Set<Point>): Viz {
+        val minX = points.minOf { it.x }
+        val minY = points.minOf { it.y }
+        val maxX = points.maxOf { it.x }.toInt()
+        val maxY = points.maxOf { it.y }.toInt()
+        val dx = 0 - minX.toInt()
+        val dy = 0 - minY.toInt()
+        val width = max(vizWidth, maxX + dx + 1)
+        val height = max(vizHeight, maxY + dy + 1)
+        val map = Array(width) { Array(height) { Tile() } }
+        points.forEach {
+            with(map[it.x.toInt() + dx][it.y.toInt() + dy]) {
+                backgroundColor = Color.Gray
+            }
+        }
+        map[location.x.toInt() + dx][location.y.toInt() + dy].borderColor = Color.Yellow
+        return Viz(progress, width, height).apply {
+            grid(0, 0, map)
+        }
+    }
+
 
     data class Step(val turn: Turn, val amount: Int)
 }
