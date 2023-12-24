2d79197d9cf6325f10ec3ad5528a15d180407adc
axl <axelvondreden@github.com>
2023-12-24 01:42:52 +0100

    changes

--- src/main/kotlin/y2016/Day08.kt
+++ src/main/kotlin/y2016/Day08.kt
@@ -3,59 +3,75 @@ package y2016
 import Day
 import utils.copy
 
-class Day08 : Day<Boolean>(2016, 8) {
+class Day08 : Day<List<Day08.Instruction>>(2016, 8) {
 
-    private val screen = Array(50) { BooleanArray(6) }
-
-    override suspend fun List<String>.parse(): Boolean {
-        forEach { it.split(" ").evaluate() }
-        return true
+    override suspend fun List<String>.parse() = map { line ->
+        line.split(" ").let { s ->
+            when (s[0]) {
+                "rect" -> s[1].split("x").let { Instruction.Rect(it[0].toInt(), it[1].toInt()) }
+                "rotate" -> when (s[1]) {
+                    "row" -> Instruction.RotateRow(s[2].split("=")[1].toInt(), s[4].toInt())
+                    "column" -> Instruction.RotateColumn(s[2].split("=")[1].toInt(), s[4].toInt())
+                    else -> error("big fail")
+                }
+                else -> error("big fail")
+            }
+        }
     }
 
-    override suspend fun solve1(input: Boolean) = screen.sumOf { booleans -> booleans.count { it } }
+    override suspend fun solve1(input: List<Instruction>): Int {
+        val screen = Array(50) { BooleanArray(6) }
+        input.forEach {
+            it.apply(screen)
+        }
+        return screen.sumOf { booleans -> booleans.count { it } }
+    }
 
-    override suspend fun solve2(input: Boolean) = if (printScreen() == control) "RURUCEOEIL" else ""
+    override suspend fun solve2(input: List<Instruction>): String {
+        val screen = Array(50) { BooleanArray(6) }
+        input.forEach {
+            it.apply(screen)
+        }
+        return if (screen.printScreen() == CONTROL) "RURUCEOEIL" else ""
+    }
 
-    private fun List<String>.evaluate() {
-        val old = screen.copy()
-        when (get(0)) {
-            "rect" -> {
-                val a = get(1).split("x")[0].toInt()
-                val b = get(1).split("x")[1].toInt()
+    sealed class Instruction {
+        abstract fun apply(map: Array<BooleanArray>)
+
+        data class Rect(val a: Int, val b: Int) : Instruction() {
+            override fun apply(map: Array<BooleanArray>) {
                 (0 until a).forEach { x ->
                     (0 until b).forEach { y ->
-                        screen[x][y] = true
+                        map[x][y] = true
                     }
                 }
             }
-            "rotate" -> {
-                when (get(1)) {
-                    "row" -> {
-                        val row = get(2).split("=")[1].toInt()
-                        val shift = get(4).toInt()
-                        screen.indices.forEach { x ->
-                            val newX = (x + shift) % screen.size
-                            screen[newX][row] = old[x][row]
-                        }
-                    }
-                    "column" -> {
-                        val col = get(2).split("=")[1].toInt()
-                        val shift = get(4).toInt()
-                        screen[0].indices.forEach { y ->
-                            val newY = (y + shift) % screen[0].size
-                            screen[col][newY] = old[col][y]
-                        }
-                    }
+        }
+        data class RotateRow(val y: Int, val shift: Int) : Instruction() {
+            override fun apply(map: Array<BooleanArray>) {
+                val old = map.copy()
+                map.indices.forEach { x ->
+                    val newX = (x + shift) % map.size
+                    map[newX][y] = old[x][y]
+                }
+            }
+        }
+        data class RotateColumn(val x: Int, val shift: Int) : Instruction() {
+            override fun apply(map: Array<BooleanArray>) {
+                val old = map.copy()
+                map[0].indices.forEach { y ->
+                    val newY = (y + shift) % map[0].size
+                    map[x][newY] = old[x][y]
                 }
             }
         }
     }
 
-    private fun printScreen(): String {
+    private fun Array<BooleanArray>.printScreen(): String {
         var ret = "\n"
-        screen[0].indices.forEach { y ->
-            screen.indices.forEach { x ->
-                ret += if (screen[x][y]) '#' else ' '
+        this[0].indices.forEach { y ->
+            indices.forEach { x ->
+                ret += if (this[x][y]) '#' else ' '
             }
             ret += "\n"
         }
@@ -63,7 +79,7 @@ class Day08 : Day<Boolean>(2016, 8) {
     }
 
     private companion object {
-        private const val control = """
+        private const val CONTROL = """
 ###  #  # ###  #  #  ##  ####  ##  ####  ### #    
 #  # #  # #  # #  # #  # #    #  # #      #  #    
 #  # #  # #  # #  # #    ###  #  # ###    #  #    
