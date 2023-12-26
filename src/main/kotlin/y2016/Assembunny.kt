236dc0e09530302742b58f78eb8ded4a8e56793f
axelvondreden <axelvondreden@gmail.com>
2023-12-26 14:13:42 +0100

    changes

--- src/main/kotlin/y2016/Assembunny.kt
+++ src/main/kotlin/y2016/Assembunny.kt
@@ -1,5 +1,8 @@
 package y2016
 
+import androidx.compose.ui.graphics.Color
+import runner.compose.VizGrid
+
 class Assembunny(private val register: MutableMap<String, Int>) {
 
     fun run(instructions: MutableList<String>, onOutput: (Int) -> Boolean = { true }) {
@@ -48,5 +51,57 @@ class Assembunny(private val register: MutableMap<String, Int>) {
         "cpy" -> "jnz"
         "tgl" -> "inc"
         else -> throw RuntimeException("oh no!")
+    }
+
+    suspend fun runViz(instructions: MutableList<String>, onOutput: (Int) -> Boolean = { true }, onProgress: suspend (VizGrid) -> Unit) {
+        var index = 0
+        while (index < instructions.size) {
+            val split = instructions[index].split(" ")
+            val cmd = split[0]
+            val arg1 = split[1]
+            val arg2 = split.getOrNull(2)
+            when (cmd) {
+                "cpy" -> {
+                    register[arg2!!] = arg1.toIntOrNull() ?: register[arg1]!!
+                    index++
+                }
+                "inc" -> {
+                    register[arg1] = register[arg1]!! + 1
+                    index++
+                }
+                "dec" -> {
+                    register[arg1] = register[arg1]!! - 1
+                    index++
+                }
+                "jnz" -> {
+                    if ((arg1.toIntOrNull() ?: register[arg1]!!) != 0) index += arg2!!.toIntOrNull() ?: register[arg2]!! else index++
+                }
+                "tgl" -> {
+                    val move = register[arg1]!!
+                    if (index + move in instructions.indices) {
+                        val tglCmd = instructions[index + move].split(" ")[0]
+                        instructions[index + move] = instructions[index + move].replace(tglCmd, tglCmd.toggle())
+                    }
+                    index++
+                }
+                "out" -> {
+                    if (onOutput.invoke(arg1.toIntOrNull() ?: register[arg1]!!)) index++
+                    else return
+                }
+            }
+            val viz = VizGrid(null, 20, 20).apply {
+                register.entries.forEachIndexed { index1, (t, u) ->
+                    text(0, index1, "$t:$u")
+                    border(2 to index1, (2 + u.toString().length - 1) to index1, Color.Yellow)
+                }
+                (-10..9).forEach {
+                    if (index + it in instructions.indices) {
+                        text(10, 10 + it, instructions[index + it])
+                    }
+                }
+                backgroundColor(10 to 10, 19 to 10, Color.Gray)
+            }
+            onProgress(viz)
+        }
     }
 }
--- src/main/kotlin/y2016/Day12.kt
+++ src/main/kotlin/y2016/Day12.kt
@@ -1,12 +1,11 @@
 package y2016
 
-import Day
+import DayViz
+import runner.compose.VizGrid
 
-class Day12 : Day<List<String>>(2016, 12) {
+class Day12 : DayViz<List<String>, VizGrid>(2016, 12) {
 
     override suspend fun List<String>.parse() = this
-
-    private val register = mutableMapOf("a" to 0, "b" to 0, "c" to 0, "d" to 0)
 
     override suspend fun solve1(input: List<String>): Int {
         val register = mutableMapOf("a" to 0, "b" to 0, "c" to 0, "d" to 0)
@@ -17,6 +16,22 @@ class Day12 : Day<List<String>>(2016, 12) {
     override suspend fun solve2(input: List<String>): Int {
         val register = mutableMapOf("a" to 0, "b" to 0, "c" to 1, "d" to 0)
         Assembunny(register).run(input.toMutableList())
+        return register["a"]!!
+    }
+
+    override fun initViz1(input: List<String>) = VizGrid(null, 20, 20)
+
+    override fun initViz2(input: List<String>) = VizGrid(null, 20, 20)
+
+    override suspend fun solve1Visualized(input: List<String>, onProgress: suspend (VizGrid) -> Unit): Any {
+        val register = mutableMapOf("a" to 0, "b" to 0, "c" to 0, "d" to 0)
+        Assembunny(register).runViz(input.toMutableList(), onProgress = onProgress)
+        return register["a"]!!
+    }
+
+    override suspend fun solve2Visualized(input: List<String>, onProgress: suspend (VizGrid) -> Unit): Any {
+        val register = mutableMapOf("a" to 0, "b" to 0, "c" to 1, "d" to 0)
+        Assembunny(register).runViz(input.toMutableList(), onProgress = onProgress)
         return register["a"]!!
     }
 }
