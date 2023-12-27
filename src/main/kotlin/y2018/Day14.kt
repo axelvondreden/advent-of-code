daceac99782efa25c6e67e7a4dcc33eb0b858d2f
axelvondreden <axelvondreden@gmail.com>
2023-12-27 11:31:05 +0100

    changes

--- src/main/kotlin/y2018/Day14.kt
+++ src/main/kotlin/y2018/Day14.kt
@@ -3,46 +3,35 @@ package y2018
 import Day
 
 class Day14 : Day<Int>(2018, 14) {
-
     override suspend fun List<String>.parse() = first().toInt()
 
-    override suspend fun solve1(input: Int): String {
-        val scores = mutableListOf(3, 7)
-        var index1 = 0
-        var index2 = 1
-        while (scores.size < input + 10) {
-            val rec1 = scores[index1]
-            val rec2 = scores[index2]
-            scores.addAll((rec1 + rec2).toString().map { it.toString().toInt() })
-            index1 += rec1 + 1
-            index1 %= scores.size
-            index2 += rec2 + 1
-            index2 %= scores.size
-        }
-        return scores.takeLast(10).joinToString("")
-    }
+    override suspend fun solve1(input: Int) = recipes { it.size == input + 10 }.takeLast(10).joinToString("")
 
-    override suspend fun solve2(input: Int): Int {
-        val scores = mutableListOf(3, 7)
-        var index1 = 0
-        var index2 = 1
-        while (true) {
-            val rec1 = scores[index1]
-            val rec2 = scores[index2]
-            (rec1 + rec2).toString().map { it.toString().toInt() }.forEach {
-                scores += it
-                if (scores.check(input)) {
-                    return scores.size - input.toString().length
+    override suspend fun solve2(input: Int) = recipes { it.endsWith(input.asDigits()) }.size - input.asDigits().size
+
+    private fun recipes(stopCondition: (List<Int>) -> Boolean): List<Int> {
+        val history = mutableListOf(3, 7)
+        var elf1 = 0
+        var elf2 = 1
+        var stop = false
+
+        while (!stop) {
+            val nextValue = history[elf1] + history[elf2]
+            nextValue.asDigits().forEach {
+                if (!stop) {
+                    history.add(it)
+                    stop = stopCondition(history)
                 }
             }
-            index1 += rec1 + 1
-            index1 %= scores.size
-            index2 += rec2 + 1
-            index2 %= scores.size
+            elf1 = (elf1 + history[elf1] + 1) % history.size
+            elf2 = (elf2 + history[elf2] + 1) % history.size
         }
+        return history
     }
 
-    private fun List<Int>.check(input: Int): Boolean {
-        return takeLast(input.toString().length).joinToString("") == input.toString()
-    }
+    private fun Int.asDigits() = toString().map { it.toString().toInt() }
+
+    private fun List<Int>.endsWith(other: List<Int>): Boolean =
+        if (this.size < other.size) false
+        else this.slice(this.size - other.size until this.size) == other
 }
